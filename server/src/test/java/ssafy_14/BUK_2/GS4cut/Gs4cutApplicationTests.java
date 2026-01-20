95cad8c94beb1320f5079dd25216fbe6c65dd9d4
송성현 <99pyues@naver.com>
2026-01-20 12:10:54 +0900

    Feature : backend 최초설정

---
+++ server/src/test/java/ssafy_14/BUK_2/GS4cut/Gs4cutApplicationTests.java
@@ -0,0 +1,13 @@
+package ssafy_14.BUK_2.GS4cut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gs4cutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
